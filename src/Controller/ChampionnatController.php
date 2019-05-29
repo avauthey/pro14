9aac9ce2aaa5af16d224fd52be59701039a6126a
avauthey <antoine.vauthey@orange.fr>
2019-05-29 23:50:00 +0200

    Initial commit

---
+++ src/Controller/ChampionnatController.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * Description of ChampionnatController
+ *
+ * @author Antoine
+ */
+namespace App\Controller;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ChampionnatController {
+    /**
+     * @Route("/lucky/number")
+    */
+    public function number()
+    {
+        $number = random_int(0, 100);
+
+        return new Response(
+            '<html><body>Lucky number: '.$number.'</body></html>'
+        );
+    }
+}
